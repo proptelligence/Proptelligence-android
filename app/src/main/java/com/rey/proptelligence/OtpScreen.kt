4bf20ed4a065c55851df01605e8baf71de9bb825
Shreyas Patil <shreyassp002@gmail.com>
2024-06-11 23:30:19 +0530

    Revamped OTP Screen UI

--- app/src/main/java/com/rey/proptelligence/OtpScreen.kt
+++ app/src/main/java/com/rey/proptelligence/OtpScreen.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -27,7 +29,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -48,72 +53,112 @@ fun OtpScreen(navController: NavController) {
     val enterNumberColor = Color(0xFF545151)
     val tColor = Color(0xFF2B472B)
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(bgColor),
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(bgColor)
+            .padding(top = 240.dp),
+
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Verification",
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            color = tColor
-        )
-        Spacer(modifier = Modifier.size(50.dp))
-        Text(text = "You will get OTP via SMS", color = tColor)
-        Spacer(modifier = Modifier.size(10.dp))
-        BasicTextField(
-            value = otp,
-            onValueChange = {
-                if (it.length <= 6) otp = it
-            },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        ){
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                repeat(6){
-                    index ->
-                    val number = when{
-                        index >= otp.length -> ""
-                        else -> otp[index]
-                    }
+        ) {
 
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(6.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text =number.toString(),
-                            style = MaterialTheme.typography.titleLarge,
-                            color = tColor
-                        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 50.dp),
+        ) {
+            Text(
+                text = "OTP",
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                color = textColor
+            )
+            Text(
+                text = "Verification",
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                color = textColor
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Enter the code from the sms we sent to", color = Color.Black, fontSize = 15.sp)
+            Text(text = "your phone number", color = Color.Black, fontSize = 15.sp)
+        }
 
-                        Box(modifier = Modifier
-                            .width(40.dp)
-                            .height(2.dp)
-                            .background(tColor)
-                        ){}
+        Spacer(modifier = Modifier.height(0.dp))
+
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(bgColor),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+
+
+            Spacer(modifier = Modifier.size(50.dp))
+
+            BasicTextField(
+                value = otp,
+                onValueChange = {
+                    if (it.length <= 6) otp = it
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            ){
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    repeat(6){
+                            index ->
+                        val number = when{
+                            index >= otp.length -> ""
+                            else -> otp[index]
+                        }
+
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(6.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text =number.toString(),
+                                style = MaterialTheme.typography.titleLarge,
+                                color = tColor
+                            )
+
+                            Box(modifier = Modifier
+                                .width(40.dp)
+                                .height(2.dp)
+                                .background(tColor)
+                            ){}
+                        }
                     }
                 }
             }
+
+
+            Spacer(modifier = Modifier.size(30.dp))
+            Button(
+                shape = RectangleShape,
+                modifier = Modifier
+                    .height(45.dp)
+                    .width(350.dp)
+                    .padding(start = 20.dp, end = 20.dp)
+                    .fillMaxWidth(0.8f)
+                    .clip(RoundedCornerShape(12.dp)),
+                onClick = {
+                    verifyPhoneNumberWithCode(context, storedVerificationId, otp, navController)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    if (otp.length >= 6) textColor else unfocusedColor)
+            ) {
+                Text(text = "Verify", color = Color.White)
+            }
+
         }
-
-
-        Spacer(modifier = Modifier.size(30.dp))
-        Button(
-            onClick = {
-               verifyPhoneNumberWithCode(context, storedVerificationId, otp, navController)
-            },
-            colors = ButtonDefaults.buttonColors(
-                if (otp.length >= 6) tColor else Color.Gray)
-        ) {
-            Text(text = "Verify", color = Color.White)
-        }
-
     }
-}
+    }
+
+
 
 @Preview(showBackground = true)
 @Composable
